f81cbf4b8b2791e2122685c18361cc516ec5eebc
Sandip Maity <121006934+Sandip162@users.noreply.github.com>
2022-12-25 14:30:59 +0530

    Create LeepYear.c

---
+++ LeepYear.c
@@ -0,0 +1,13 @@
+#include<stu ZZdio.h>
+Int main () {
+    int year;
+    printf("Enter The Year  :  ");
+    scanf("%d",&year);
+
+    if(year % 4 != 0 || year % 400 != 0) {
+         printf("%d year is not an Leep Year \n",year);
+    } else if (year % 4 == 0) {
+         printf("%d year is an Leep Year \n",year);
+    }
+    return 0;
+}
